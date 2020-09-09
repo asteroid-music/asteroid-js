9874edd0dfac4e24387200a73cb048096303989d
JrTheDragon01 <www.aereregion.webs.com@live.co.uk>
2020-09-09 10:02:26 +0100

    Removed mock song data for queue and added axios calls; added state to <AsteroidQueue>

--- src/tsx/body-content/queue.tsx
+++ src/tsx/body-content/queue.tsx
@@ -1,46 +1,104 @@
 //Basic imports
 import React from 'react';
-import { Typography, Container, List } from '@material-ui/core'
+import { Typography, Container, List, Button } from '@material-ui/core'
 import axios from 'axios'
 
 //Relative local imports
 import { AsteroidSongItem, SongInterface } from '../song-item.tsx'
 
-//Mock data for testing purposes; will be removed later
-const mock_json_data = [
-    {
-        artist: "Artist1",
-        song: "Song1",
-        duration: 225.3,
-        album: "Album1",
-        id: 12,
-        votes: 1
-    },
-    {
-        artist: "Artist2",
-        song: "Song2",
-        duration: 963.21,
-        album: "Album2",
-        id: 24,
-        votes: 2
-    }
-]
+/**
+ * State for the <AsteroidQueue> component
+ */
+interface QueueState {
+    queueData?: SongInterface[],
+    requestFailureString?: string
+}
 
 /**
  * <AsteroidQueue> component for main body
  */
-function AsteroidQueue(props: {}) {
-    mock_json_data.sort((a,b) => {return b.votes - a.votes;}) //sorting by vote order
-    return <Container>
-        <Typography>
-            Currently playing song: Unknown
-        </Typography>
-        <List>
-            {mock_json_data.map((jsonitem: SongInterface) => {
-                return <AsteroidSongItem key={jsonitem.id} {...jsonitem}/>
-            })}
-        </List>
-    </Container>
+class AsteroidQueue extends React.Component<{},QueueState> {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this._refreshQueue();
+    }
+
+    _refreshQueue() {
+        axios.get("/queue").then(
+            (response: any) => {
+                this.setState({
+                    queueData: response.data
+                });
+            }
+        ).catch(
+            (error) => {
+                this.setState({
+                    requestFailureString: error.message
+                });
+            }
+        )
+    }
+
+    refreshQueue() {
+        this.setState({
+            queueData: null,
+            requestFailureString: null
+        });
+        this._refreshQueue();
+    }
+
+    render() {
+        let queueData = this.state.queueData;
+        let requestFailureString = this.state.requestFailureString;
+
+        const refreshButton = <Button
+            onClick={() => {this.refreshQueue();}}
+        >
+            Refresh queue
+        </Button>
+
+        let listInfoItem = <Typography>Requesting queue data...</Typography>
+        let refreshButtonOrNull;
+
+        if (queueData) {
+            //Check if any data present
+            if (queueData.length > 0) {
+                //Sort by number of votes
+                queueData.sort(function(a,b){return b.votes-a.votes;})
+                //Remove all songs with <= 0 votes
+                let lastIndex: number = queueData.findIndex(function(song){return song.votes <= 0;});
+                lastIndex = (lastIndex > 0) ? lastIndex + 1 : queueData.length;
+                lastIndex = (queueData[lastIndex-1].votes <= 0) ? lastIndex-1 : lastIndex;
+                queueData = queueData.slice(0,lastIndex);
+                if (queueData[0].votes == 0) {
+                    listInfoItem = <Typography>No songs found in the queue! Get upvoting!</Typography>
+                    refreshButtonOrNull = refreshButton;
+                } else {
+                    listInfoItem = <List>
+                        {queueData.map((jsonitem: SongInterface) => {
+                            return <AsteroidSongItem key={jsonitem.id} {...jsonitem}/>
+                        })}
+                    </List>
+                    refreshButtonOrNull = refreshButton;
+                }
+            } else {
+                listInfoItem = <Typography>No songs found in the queue! Get upvoting!</Typography>
+                refreshButtonOrNull = refreshButton;
+            }
+        } else if (requestFailureString){
+            listInfoItem = <Typography>Error when loading queue: {requestFailureString}</Typography>
+            refreshButtonOrNull = refreshButton;
+        }
+
+        return <Container>
+            <Typography>
+                Currently playing song: Unknown
+            </Typography>
+            {listInfoItem}
+            {refreshButtonOrNull}
+        </Container>
+    }
 }
 
 
