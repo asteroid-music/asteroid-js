d93527242a3df1dab7db529c886bbaee538fb049
JrTheDragon01 <www.aereregion.webs.com@live.co.uk>
2020-09-09 19:07:11 +0100

    Added axios calls for upvoting (downvoting currently not implemented serverside) and clear selection formatting for upvote/downvote buttons

--- src/tsx/song-item.tsx
+++ src/tsx/song-item.tsx
@@ -1,5 +1,6 @@
 //Basic imports
 import React from 'react';
+import type {ElementType} from 'react';
 import { ListItem, ListItemText, Typography } from '@material-ui/core';
 import { red, green } from '@material-ui/core/colors';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
@@ -7,6 +8,7 @@ import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import UnfoldMore from '@material-ui/icons/UnfoldMore';
 import UnfoldLess from '@material-ui/icons/UnfoldLess';
 import IconButton from '@material-ui/core/IconButton';
+import axios from 'axios';
 
 /**
  * Interface for a song item.
@@ -43,7 +45,8 @@ interface SongItemProps {
 }
 
 interface SongItemState {
-    unfolded: boolean
+    unfolded: boolean,
+    voteState: number
 }
 
 /**
@@ -54,7 +57,8 @@ class AsteroidSongItem extends React.Component<SongItemProps,SongItemState> {
     constructor(props) {
         super(props);
         this.state = {
-            unfolded: this.props.unfolded
+            unfolded: this.props.unfolded,
+            voteState: 0
         };
     }
 
@@ -72,39 +76,90 @@ class AsteroidSongItem extends React.Component<SongItemProps,SongItemState> {
         return song.artist + " - " + song.album + " - " + song.duration;
     }
 
+    upvoteCallback() {
+        let voteState = this.state.voteState;
+        if (voteState == 1) {
+            this.setState({voteState:0});
+        } else {
+            axios.post<{message: string}>("/queue",{id:this.props.song?.id}).then(
+                (response) => {
+                }
+            ).catch(
+                (error) => {
+                    this.setState({voteState:voteState});
+                }
+            );
+            this.setState({voteState:1});
+        }
+    }
+
+    downvoteCallback() {
+        let voteState = this.state.voteState;
+        if (voteState == -1) {
+            axios.post<{message: string}>("/queue",{id:this.props.song?.id}).then(
+                (response) => {
+                }
+            ).catch(
+                (error) => {
+                    this.setState({voteState:-1});
+                }
+            );
+            this.setState({voteState:0});
+        } else {
+            this.setState({voteState:-1});
+        }
+    }
+
     render () {
         let song = this.props.song;
 
         let unfolded = this.state.unfolded;
+        let voteState = this.state.voteState;
 
         let textSecondary = unfolded
             ? this.unfoldedSecondaryNode()
-            : this.foldedSecondaryText()
+            : this.foldedSecondaryText();
 
         let foldButton = unfolded
             ? <IconButton onClick={() => {this.setState({unfolded:false});}}><UnfoldMore /></IconButton>
             : <IconButton onClick={() => {this.setState({unfolded:true});}}><UnfoldLess /></IconButton>
 
         let otherProps = unfolded
-            ? {}
+            ? {
+                secondaryTypographyProps: {component:"div" as ElementType}
+            }
             : {
                 primaryTypographyProps: {noWrap:true},
                 secondaryTypographyProps: {noWrap:true}
-            }
+            };
 
-        let upButton = this.props.voteButtons && <IconButton>
+        let upButtonStyle = (voteState === 1)
+            ? {backgroundColor: green[100]}
+            : {};
+
+        let downButtonStyle = (voteState === -1)
+            ? {backgroundColor: red[100]}
+            : {};
+
+        let upButton = this.props.voteButtons && <IconButton
+            style={upButtonStyle}
+            onClick={() => {this.upvoteCallback();}}
+        >
             <ArrowUpwardIcon
                 style={{ color: green[500] }}
             />
         </IconButton>
 
-        let downButton = this.props.voteButtons && <IconButton>
+        let downButton = this.props.voteButtons && <IconButton
+            style={downButtonStyle}
+            onClick={() => {this.downvoteCallback();}}
+        >
             <ArrowDownwardIcon
                 style={{ color: red[500] }}
             />
         </IconButton>
 
-        let votesInfo = song.votes && <Typography>{song.votes}</Typography>
+        let votesInfo = song.votes && <Typography>{song.votes+voteState}</Typography>
 
         return <ListItem>
             <ListItemText
