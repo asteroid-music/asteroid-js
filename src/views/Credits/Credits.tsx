1fbffbac8ea59a7dd1f7ac8270fde4a68b9a1b1f
OneSlightWeirdo <www.aereregion.webs.com@live.co.uk>
2020-11-22 12:03:14 +0000

    Added basic info to Credits page

--- src/views/Credits/Credits.tsx
+++ src/views/Credits/Credits.tsx
@@ -1,14 +1,28 @@
 //Basic imports
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 /**
  * <AsteroidCredits> component for main body
  */
 function AsteroidCredits(props: {}) {
-    return <Typography variant="body1">
-        TODO: add credits here!
-    </Typography>
+    return <Container>
+        <Typography variant="body1">
+            Asteroid built by <a href="https://github.com/dustpancake" target="_blank">
+                Fergus Baker
+            </a>
+            {" and "}
+            <a href="https://github.com/JR-Mitchell" target="_blank">
+                JR Mitchell
+            </a>
+            <br/>
+            {"GitHub repo at: "}
+            <a href="https://github.com/asteroid-music" target="_blank">
+                asteroid-music
+            </a>
+        </Typography>
+    </Container>
 }
 
 
